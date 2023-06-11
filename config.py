c9a702b2deccdb6ebd84227a8f98c0fd3d63ff6f
SARKAROP123 <111958063+SARKAROP123@users.noreply.github.com>
2023-06-11 15:25:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,11 +7,11 @@ load_dotenv()
 API_ID = int(getenv("API_ID", "27353035"))
 API_HASH = getenv("API_HASH", "cf2a75861140ceb746c7796e07cbde9e")
 BOT_TOKEN = getenv("BOT_TOKEN", None)
-OWNER_ID = int(getenv("OWNER_ID", "5350929381"))
+OWNER_ID = int(getenv("OWNER_ID", "6219965257"))
 MONGO_URL = getenv("MONGO_URL", None)
-SUPPORT_GRP = getenv("SUPPORT_GRP", "Nexa_Verse")
-UPDATE_CHNL = getenv("UPDATE_CHNL", "DadEyeBotz")
-OWNER_USERNAME = getenv("OWNER_USERNAME", "Dev_Arora_0981")
+SUPPORT_GRP = getenv("SUPPORT_GRP", "MY_ABOUT_HERE")
+UPDATE_CHNL = getenv("UPDATE_CHNL", "MY_ABOUT_HERE")
+OWNER_USERNAME = getenv("OWNER_USERNAME", "ll_SARKAR_OP_ll")
 
 # Random Start Images
 IMG = [
