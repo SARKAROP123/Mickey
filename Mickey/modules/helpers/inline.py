041dc1535883b1b18421e35df604fc0ec6d4a6a1
SARKAROP123 <111958063+SARKAROP123@users.noreply.github.com>
2023-10-12 14:36:51 +0530

    Update inline.py

--- Mickey/modules/helpers/inline.py
+++ Mickey/modules/helpers/inline.py
@@ -5,19 +5,35 @@ from Mickey import OWNER
 from Mickey import MickeyBot
 
 DEV_OP = [
-[       
-            InlineKeyboardButton(
+    [
+        InlineKeyboardButton(
             text="🥵𝐀𝐝𝐝 𝐌𝐞 𝐘𝐨𝐮𝐫 𝐆𝐫𝐨𝐮𝐩🥵",
             url=f"https://t.me/{MickeyBot.username}?startgroup=true",
         ),
-    ]
+    ],
+    [
+        InlineKeyboardButton(text="🚀 ʜᴇʟᴘ & ᴄᴍᴅs 🚀", callback_data="HELP"),
+    ],
 ]
-            
+
 PNG_BTN = [
     [
         InlineKeyboardButton(
             text="🥵𝐀𝐝𝐝 𝐌𝐞 𝐘𝐨𝐮𝐫 𝐆𝐫𝐨𝐮𝐩🥵",
             url=f"https://t.me/{MickeyBot.username}?startgroup=true",
-        )
+        ),
+    ],
+    [
+        InlineKeyboardButton(
+            text="✨ ᴄʟᴏsᴇ ✨",
+            callback_data="CLOSE",
+        ),
+    ],
+]
+
+
+BACK = [
+    [
+        InlineKeyboardButton(text="✨ ʙᴀᴄᴋ ✨", callback_data="BACK"),
     ],
 ]
