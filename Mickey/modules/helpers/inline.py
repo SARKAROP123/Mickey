260cfdf0511e214f6e4df8203a707c6a16840403
SARKAROP123 <111958063+SARKAROP123@users.noreply.github.com>
2023-10-12 14:22:20 +0530

    Update inline.py

--- Mickey/modules/helpers/inline.py
+++ Mickey/modules/helpers/inline.py
@@ -17,6 +17,6 @@ PNG_BTN = [
         InlineKeyboardButton(
             text="🥵𝐀𝐝𝐝 𝐌𝐞 𝐘𝐨𝐮𝐫 𝐆𝐫𝐨𝐮𝐩🥵",
             url=f"https://t.me/{MickeyBot.username}?startgroup=true",
-        ),
+        )
     ],
 ]
