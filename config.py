10389ff81d0b4032ea936a1575da04001673f026
SARKAROP123 <111958063+SARKAROP123@users.noreply.github.com>
2024-10-12 09:22:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ OWNER_ID = int(getenv("OWNER_ID", 6919772024)
 MONGO_URL = getenv("MONGO_URL", None)
 SUPPORT_GRP = getenv("SUPPORT_GRP", "TG_NAME_STYLE")
 UPDATE_CHNL = getenv("UPDATE_CHNL", "TKS_JOIN")
-OWNER_USERNAME = getenv("OWNER_USERNAME", "ghghhghgggvbb)
+OWNER_USERNAME = getenv("OWNER_USERNAME", "chin_tabak_dum_dum")
 
 # Random Start Images
 IMG = [
