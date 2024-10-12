a55cb6b16fc0e7597a46724c37f274ee00bda798
SARKAROP123 <111958063+SARKAROP123@users.noreply.github.com>
2024-10-12 09:03:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,11 +7,11 @@ load_dotenv()
 API_ID = int(getenv("API_ID", 27353035))
 API_HASH = getenv("API_HASH", "cf2a75861140ceb746c7796e07cbde9e")
 BOT_TOKEN = getenv("BOT_TOKEN", None)
-OWNER_ID = int(getenv("OWNER_ID", 6402115032))
+OWNER_ID = int(getenv("OWNER_ID", 6919772024)
 MONGO_URL = getenv("MONGO_URL", None)
 SUPPORT_GRP = getenv("SUPPORT_GRP", "TG_NAME_STYLE")
 UPDATE_CHNL = getenv("UPDATE_CHNL", "TKS_JOIN")
-OWNER_USERNAME = getenv("OWNER_USERNAME", "ll_SARKAR_BABE_ll")
+OWNER_USERNAME = getenv("OWNER_USERNAME", "ll_SARKAR_MERA_BABU_ll)
 
 # Random Start Images
 IMG = [
