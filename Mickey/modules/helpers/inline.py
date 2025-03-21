ce335d03e12823c4c297a36946d52e3037450306
SARKAROP123 <111958063+SARKAROP123@users.noreply.github.com>
2025-03-21 13:17:23 +0530

    Update inline.py

--- Mickey/modules/helpers/inline.py
+++ Mickey/modules/helpers/inline.py
@@ -6,30 +6,30 @@ from Mickey import MickeyBot
 
 DEV_OP = [
     [
-        InlineKeyboardButton(text="❮🥀𝐎𝐰𝐧𝐞𝐫🥀❯", user_id=OWNER),
-        InlineKeyboardButton(text="❮⭐𝐔𝐩𝐝𝐚𝐭𝐞🌟❯", url=f"https://t.me/{SUPPORT_GRP}"),
+        InlineKeyboardButton(text="𝙊𝙒𝙉𝙀𝙍", user_id=OWNER),
+        InlineKeyboardButton(text="𝙐𝙋𝘿𝘼𝙏𝙀", url=f"https://t.me/{SUPPORT_GRP}"),
     ],
     [
         InlineKeyboardButton(
-            text="❮𝐀𝐝𝐝 𝐘𝐨𝐮 𝐆𝐫𝐨𝐮𝐩❯",
+            text="❮𝘼𝘿𝘿 𝙔𝙊𝙐𝙍 𝙂𝙍𝙊𝙐𝙋❯",
             url=f"https://t.me/{MickeyBot.username}?startgroup=true",
         ),
     ],
     [
-        InlineKeyboardButton(text="❮🍷𝐇𝐞𝐥𝐩🍷❯", callback_data="HELP"),
+        InlineKeyboardButton(text="❮𝙃𝙀𝙇𝙋❯", callback_data="HELP"),
     ],
 ]
 
 PNG_BTN = [
     [
         InlineKeyboardButton(
-            text="❮💥𝐀𝐃𝐃 𝐌𝐄 𝐘𝐎𝐔𝐑 𝐆𝐑𝐎𝐔𝐏💥❯",
+            text="❮𝐀𝐃𝐃 𝐌𝐄 𝐘𝐎𝐔𝐑 𝐆𝐑𝐎𝐔𝐏❯",
             url=f"https://t.me/{MickeyBot.username}?startgroup=true",
         ),
     ],
     [
         InlineKeyboardButton(
-            text="❮💥𝗛𝗘𝗟𝗣💥❯",
+            text="❮𝗛𝗘𝗟𝗣❯",
             callback_data="CLOSE",
         ),
     ],
@@ -38,64 +38,64 @@ PNG_BTN = [
 
 BACK = [
     [
-        InlineKeyboardButton(text="❮💥𝗕𝗔𝗖𝗞💥❯", callback_data="BACK"),
+        InlineKeyboardButton(text="❮𝗕𝗔𝗖𝗞❯", callback_data="BACK"),
     ],
 ]
 
 
 HELP_BTN = [
     [
-        InlineKeyboardButton(text="❮💥𝗖𝗛𝗔𝗧𝗕𝗢𝗧💥❯", callback_data="CHATBOT_CMD"),
+        InlineKeyboardButton(text="❮𝗖𝗛𝗔𝗧𝗕𝗢𝗧❯", callback_data="CHATBOT_CMD"),
         InlineKeyboardButton(text="𝗧𝗢𝗢𝗟𝗦", callback_data="TOOLS_DATA"),
     ],
     [
-        InlineKeyboardButton(text="❮💥𝗕𝗔𝗖𝗞💥❯", callback_data="BACK"),
-        InlineKeyboardButton(text="❮💥𝗖𝗟𝗢𝗦𝗘💥❯", callback_data="CLOSE"),
+        InlineKeyboardButton(text="❮𝗕𝗔𝗖𝗞❯", callback_data="BACK"),
+        InlineKeyboardButton(text="❮𝗖𝗟𝗢𝗦𝗘❯", callback_data="CLOSE"),
     ],
 ]
 
 
 CLOSE_BTN = [
     [
-        InlineKeyboardButton(text="❮💥𝗖𝗟𝗢𝗦𝗘💥❯", callback_data="CLOSE"),
+        InlineKeyboardButton(text="❮𝗖𝗟𝗢𝗦𝗘❯", callback_data="CLOSE"),
     ],
 ]
 
 
 CHATBOT_ON = [
     [
-        InlineKeyboardButton(text="❮💥𝗘𝗡𝗔𝗕𝗟𝗘💥❯", callback_data=f"addchat"),
-        InlineKeyboardButton(text="❮💥𝗗𝗶𝗦𝗔𝗕𝗟𝗘💥❯", callback_data=f"rmchat"),
+        InlineKeyboardButton(text="❮𝗘𝗡𝗔𝗕𝗟𝗘❯", callback_data=f"addchat"),
+        InlineKeyboardButton(text="❮𝗗𝗶𝗦𝗔𝗕𝗟𝗘❯", callback_data=f"rmchat"),
     ],
 ]
 
 
 MUSIC_BACK_BTN = [
     [
-        InlineKeyboardButton(text="❮💥𝗦𝗢𝗢𝗡💥❯", callback_data=f"soom"),
+        InlineKeyboardButton(text="❮𝗦𝗢𝗢𝗡❯", callback_data=f"soom"),
     ],
 ]
 
 S_BACK = [
     [
-        InlineKeyboardButton(text="❮💥𝗕𝗔𝗖𝗞💥❯", callback_data="SBACK"),
-        InlineKeyboardButton(text="❮💥𝗖𝗟𝗢𝗦𝗘💥❯", callback_data="CLOSE"),
+        InlineKeyboardButton(text="❮𝗕𝗔𝗖𝗞❯", callback_data="SBACK"),
+        InlineKeyboardButton(text="❮𝗖𝗟𝗢𝗦𝗘❯", callback_data="CLOSE"),
     ],
 ]
 
 
 CHATBOT_BACK = [
     [
-        InlineKeyboardButton(text="❮💥𝗕𝗔𝗖𝗞💥❯", callback_data="CHATBOT_BACK"),
-        InlineKeyboardButton(text="❮💥𝗖𝗟𝗢𝗦𝗘💥❯", callback_data="CLOSE"),
+        InlineKeyboardButton(text="❮𝗕𝗔𝗖𝗞❯", callback_data="CHATBOT_BACK"),
+        InlineKeyboardButton(text="❮𝗖𝗟𝗢𝗦𝗘❯", callback_data="CLOSE"),
     ],
 ]
 
 
 HELP_START = [
     [
-        InlineKeyboardButton(text="❮💥𝗛𝗘𝗟𝗣💥❯", callback_data="HELP"),
-        InlineKeyboardButton(text="❮💥𝗖𝗟𝗢𝗦𝗘💥❯", callback_data="CLOSE"),
+        InlineKeyboardButton(text="❮𝗛𝗘𝗟𝗣❯", callback_data="HELP"),
+        InlineKeyboardButton(text="❮𝗖𝗟𝗢𝗦𝗘❯", callback_data="CLOSE"),
     ],
 ]
 
@@ -103,24 +103,24 @@ HELP_START = [
 HELP_BUTN = [
     [
         InlineKeyboardButton(
-            text="❮💥𝗔𝗗𝗗 𝗬𝗢𝗨𝗥 𝗖𝗛𝗔𝗧💥❯", url=f"https://t.me/{MickeyBot.username}?start=help"
+            text="❮𝗔𝗗𝗗 𝗬𝗢𝗨𝗥 𝗖𝗛𝗔𝗧❯", url=f"https://t.me/{MickeyBot.username}?start=help"
         ),
-        InlineKeyboardButton(text="❮💥𝗖𝗟𝗢𝗦𝗘💥❯", callback_data="CLOSE"),
+        InlineKeyboardButton(text="❮𝗖𝗟𝗢𝗦𝗘❯", callback_data="CLOSE"),
     ],
 ]
 
 
 ABOUT_BTN = [
     [
-        InlineKeyboardButton(text="❮💥𝗝𝗢𝗜𝗡💥❯", url=f"https://t.me/{SUPPORT_GRP}"),
-        InlineKeyboardButton(text="❮💥𝗛𝗘𝗟𝗣💥❯", callback_data="HELP"),
+        InlineKeyboardButton(text="❮𝗝𝗢𝗜𝗡❯", url=f"https://t.me/{SUPPORT_GRP}"),
+        InlineKeyboardButton(text="❮𝗛𝗘𝗟𝗣❯", callback_data="HELP"),
     ],
     [
-        InlineKeyboardButton(text="❮💥𝗢𝗪𝗡𝗘𝗥💥❯", user_id=OWNER),
-        InlineKeyboardButton(text="❮💥𝗝𝗢𝗜𝗡💥❯", callback_data="https://t.me/{UPDATE_CHNL}"),
+        InlineKeyboardButton(text="❮𝗢𝗪𝗡𝗘𝗥❯", user_id=OWNER),
+        InlineKeyboardButton(text="❮𝗝𝗢𝗜𝗡❯", callback_data="https://t.me/{UPDATE_CHNL}"),
     ],
     [
-        InlineKeyboardButton(text="❮💥𝗨𝗣𝗗𝗔𝗧𝗘💥❯", url=f"https://t.me/{UPDATE_CHNL}"),
-        InlineKeyboardButton(text="❮💥𝗕𝗔𝗖𝗞💥❯", callback_data="BACK"),
+        InlineKeyboardButton(text="❮𝗨𝗣𝗗𝗔𝗧𝗘❯", url=f"https://t.me/{UPDATE_CHNL}"),
+        InlineKeyboardButton(text="❮𝗕𝗔𝗖𝗞❯", callback_data="BACK"),
     ],
 ]
